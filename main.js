e4388cf777240d24b4ce5aff0a6a953a01e77279
111USZ <161062914+111USZ@users.noreply.github.com>
2024-05-24 15:42:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let num1 = +prompt("1 sonni kiriting")
+
+let num2 = +prompt("2 sonni kiriting")
+
+let amallar = +prompt("Amallarni tanlang \n 1)qoshish \n 2)bolish \n 3)kopaytirish \n 4)ayirish")
+
+if (amallar ==1 ){
+    alert(num1+num2);
+}
+
+if (amallar ==2 ){
+    alert(num1-num2);
+}  
+
+if (amallar ==3 ){
+    alert(num1*num2);
+}
+
+if (amallar ==4 ){
+    alert(num1/num2);
+}
